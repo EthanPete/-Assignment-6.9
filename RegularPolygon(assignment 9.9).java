ac241181d2c9762493be30450f0120a4adb2b522
Ethan Chadwick <117663011+EthanPete@users.noreply.github.com>
2023-08-25 10:06:54 -0400

    Update and rename RegularPolygon.java to RegularPolygon(assignment 9.9).java

--- RegularPolygon(assignment 9.9).java
+++ RegularPolygon(assignment 9.9).java
@@ -69,3 +69,27 @@ public class RegularPolygon {
   System.out.println("Polygon 3 - Perimeter: " + polygon3.getPerimeter() + ", Area: " + polygon3.getArea());
  }
 }
+
+UML:
+-------------------------------------
+|     RegularPolygon                |
+-------------------------------------
+| - n: int                          |
+| - side: double                    |
+| - x: double                       |
+| - y: double                       |
+-------------------------------------
+| + RegularPolygon()                |
+| + RegularPolygon(n: int, side: double) |
+| + RegularPolygon(n: int, side: double, x: double, y: double) |
+| + getN(): int                     |
+| + setN(n: int): void              |
+| + getSide(): double               |
+| + setSide(side: double): void     |
+| + getX(): double                  |
+| + setX(x: double): void           |
+| + getY(): double                  |
+| + setY(y: double): void           |
+| + getPerimeter(): double          |
+| + getArea(): double               |
+-------------------------------------
