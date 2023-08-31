d5bbd8a370068b0c4cc719440802903857536abb
Ethan Chadwick <117663011+EthanPete@users.noreply.github.com>
2023-08-31 12:30:28 -0400

    Add files via upload

---
+++ MyDate.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    // No-arg constructor for current date
+    public MyDate() {
+        this(System.currentTimeMillis());
+    }
+
+    // Constructor with elapsed time
+    public MyDate(long elapsedTime) {
+        setDate(elapsedTime);
+    }
+
+    // Constructor with specified year, month, and day
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    // Getter methods
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    // Method to set a new date using elapsed time
+    public void setDate(long elapsedTime) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(elapsedTime);
+
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+}
